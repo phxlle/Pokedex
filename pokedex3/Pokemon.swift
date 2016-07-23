d72ed242a2683341ee4bdc6a6915ce64f61595c1
Jonny B <jonnyb@Jonnys-MacBook-Pro.local>
2016-07-23 00:22:45 -0700

    Lecture 2

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Jonny B on 7/23/16.
+//  Copyright © 2016 Jonny B. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+
+}
