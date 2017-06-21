73ec97e6e75c1443d1d04643abe98af4c1ef1a76
Phxlle <philippe.asselbergh@gmail.com>
2017-06-21 21:00:24 +0200

    first commit

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Jonny B on 7/24/16.
+//  Copyright © 2016 Jonny B. All rights reserved.
+//
+
+import Foundation
---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Jonny B on 7/23/16.
+//  Copyright © 2016 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
